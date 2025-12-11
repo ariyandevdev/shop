1bba9c664d17ba982f341a9116fab091bcb87924
Ariyan Yarahmadi <yarahmadiariyan@gmail.com>
2025-12-11 19:03:53 +0330

    add cart logic

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { Prisma } from "@prisma/client";
 import { prisma } from "./prisma";
 
 export async function getProductbySlug(slug: string) {
@@ -23,4 +24,81 @@ export async function getCategories() {
     },
   });
   return categories;
+}
+
+export type CartWithProducts = Prisma.CartGetPayload<{
+  include: {
+    items: {
+      include: {
+        product: true;
+      };
+    };
+  };
+}>;
+
+export type ShoppingCart = CartWithProducts & {
+  size: number;
+  subtotal: number;
+};
+
+export type CartItemWithProduct = Prisma.CartItemGetPayload<{
+  include: { product: true };
+}>;
+
+import { cookies } from "next/headers";
+
+async function findCartFromCookie(): Promise<CartWithProducts | null> {
+  const cartId = (await cookies()).get("cartId")?.value;
+  if (!cartId) {
+    return null;
+  }
+
+  return prisma.cart.findUnique({
+    where: { id: cartId },
+    include: {
+      items: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
+}
+
+export async function getCart(): Promise<ShoppingCart | null> {
+  const cart = await findCartFromCookie();
+
+  if (!cart) {
+    return null;
+  }
+
+  return {
+    ...cart,
+    size: cart.items.length,
+    subtotal: cart.items.reduce(
+      (total, item) => total + Number(item.product.price) * item.quantity,
+      0
+    ),
+  };
+}
+
+async function getOrCreateCart(): Promise<CartWithProducts> {
+  let cart = await findCartFromCookie();
+
+  if (cart) {
+    return cart;
+  }
+
+  cart = await prisma.cart.create({
+    data: {},
+    include: { items: { include: { product: true } } },
+  });
+
+  (await cookies()).set("cartId", cart.id, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+  });
+
+  return cart;
 }
---
+++ prisma/migrations/20251211141555_add_cart/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Cart" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Cart_pkey" PRIMARY KEY ("id")
+);
